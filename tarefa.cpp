2be6b29dba3847c70662f1a3f725348e0e0ee7c2
Luiz Felipe <y9lui@hotmail.com>
2020-10-04 21:35:56 -0300

    no description

--- tarefa.cpp
+++ tarefa.cpp
@@ -6,34 +6,39 @@
 #include <unistd.h>
 using namespace std;
 
+char * filename1;
+char * filename2;
+
 void * Normalize(void* obj)
 {
     
     MIL::Img  img =  *((MIL::Img *) obj);
 
-
-    MIL::imwrite(MIL::img_proc::Normalize(img),"imagem_normalizada.pgm");
+    std::cout<<filename1<<"\n";
+    MIL::imwrite(MIL::img_proc::Normalize(img),filename1);
 
 }
 
 void * Equalize(void* obj)
 {
     MIL::Img  img =  *(MIL::Img *) obj;
-    MIL::imwrite(MIL::img_proc::ImageEqualization(img),"imagem_equalizada.pgm");
+    std::cout<<filename2<<"\n";
+    MIL::imwrite(MIL::img_proc::ImageEqualization(img),filename2);
 }
 
 int main(int argc, char **argv)
 {
-    if(argc!=3)
+    if(argc!=4)
     {
-        std::cout<<"./test_load_save_image <loaded_image> <writed_image>\n";
+        std::cout<<"./test_load_save_image <loaded_image> <normalized_image_name> <equalized_image_name>\n";
         exit(0);
     }
 
    
     MIL::Img imagem = MIL::imread(argv[1]); 
 
-    
+    filename1=argv[2];
+    filename2=argv[3];
     pthread_t t[2];
 
 
